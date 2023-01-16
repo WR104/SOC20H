bcbef7f9a5a4fea2e110ca88aa7073601cd77fbe
WR104 <252600486L@gmail.com>
2023-01-16 12:44:25 -0800

    app

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-let url = 'https://filebin.net/xrom6qfi37vnnp4i/test.json'
+let url = 'https://cors-anywhere.herokuapp.com/https://filebin.net/xrom6qfi37vnnp4i/test.json'
+
 const j = fetch(url)
     .then((response) => response.json())
     .then((e) => {
@@ -6,33 +7,6 @@ const j = fetch(url)
     })
 
 var el = document.createElement('html')
-
-let readId = "12cTiV6swRJRDiXlCgTbupr4T-UF_eWIt"
-async function downloadFile(realFileId) {
-    // Get credentials and build service
-    // TODO (developer) - Use appropriate auth mechanism for your app
-  
-    const {GoogleAuth} = require('google-auth-library');
-    const {google} = require('googleapis');
-  
-    const auth = new GoogleAuth({
-      scopes: 'https://www.googleapis.com/auth/drive',
-    });
-    const service = google.drive({version: 'v3', auth});
-  
-    fileId = realFileId;
-    try {
-      const file = await service.files.get({
-        fileId: fileId,
-        alt: 'media',
-      });
-      console.log(file.status);
-      return file.status;
-    } catch (err) {
-      // TODO(developer) - Handle error
-      throw err;
-    }
-  }
 
 inputT = []
 document.getElementById('fileInput').addEventListener('change', function selectedFileChanged() {
@@ -93,36 +67,4 @@ document.getElementById('fileInput').addEventListener('change', function selecte
                 canvas.appendChild(newlist)
             }
         })
-
 })
-// const print = async () => {
-//     const T = await j
-
-//     inputT = await get()
-//     //T.questions.push({})
-//     console.log(T.questions)
-//     console.log(inputT)
-
-//     questions = T.questions
-//     var canvas = document.getElementById('canvas')
-//     for (const i in questions) {
-//         var question = questions[i]
-//         var newlist = document.createElement("ul")
-//         var header = document.createElement('a')
-//         var newline = document.createElement('br')
-//         newlist.appendChild(header)
-//         header.textContent = question.describe
-
-//         for (const a in question.answers) {
-//             var answer = question.answers[a]
-//             var newa = document.createElement('li')
-//             var describe = document.createElement('a' + a)
-//             newa.appendChild(describe)
-//             newlist.appendChild(newa)
-//             describe.textContent = answer.string + (answer.check === true ? ' ✔️' : answer.check === false ? ' ❌' : '')
-//         }
-
-//         newlist.appendChild(newline)
-//         canvas.appendChild(newlist)
-//     }
-// }
